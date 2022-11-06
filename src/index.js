6273707575e8f900e5e19a811a862df7f1803052
AnastasiiaBeltsova <anastasiya.b145@gmail.com>
2022-11-06 20:22:15 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const data = [undefined, 2, 3, 4, 5, 'sd'];
+
+function getArguments(a, b, ...c) {
+    if (data.length < 5) {
+        console.log('Error');
+    }
+    else if (data[0] === undefined) {
+        data[0] = 'error';
+    }
+    
+    const arg = data.slice(0, 3);
+    const newArg = arg.join('-');
+    console.log(newArg)
+
+    const array = data.slice(3, 6)
+    let map = new Map();
+    map.set('0', array[0]);
+    map.set('1', array[1]);
+    map.set('2', array[2]);
+    let obj = Object.fromEntries(map.entries());
+
+    console.log(obj)
+
+    return data;
+}
+getArguments(data);
